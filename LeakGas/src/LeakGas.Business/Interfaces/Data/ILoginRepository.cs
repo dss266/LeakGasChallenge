5ed574ee9f720d22f3d0bac004539996c129941e
Ruan Araújo <ruan.araujo@orsoftware.com.br>
2021-09-09 15:38:30 -0300

    criado repositório de login

---
+++ LeakGas/src/LeakGas.Business/Interfaces/Data/ILoginRepository.cs
@@ -0,0 +1,11 @@
+﻿using LeakGas.Business.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeakGas.Business.Interfaces.Data
+{
+    public interface ILoginRepository : IRepository<Login>
+    {
+    }
+}
---
+++ LeakGas/src/LeakGas.Data/Repositories/LoginRepository.cs
@@ -0,0 +1,13 @@
+﻿using LeakGas.Business.Interfaces.Data;
+using LeakGas.Business.Models;
+using LeakGas.Data.Context;
+
+namespace LeakGas.Data.Repositories
+{
+    public class LoginRepository : Repository<Login>, ILoginRepository
+    {
+        protected LoginRepository(LeakGasContext db) : base(db)
+        {
+        }
+    }
+}
