c0d3f903a0e26eac3ca85fc60beb62f09bb4205b
Ruan Araújo <ruan.araujo@orsoftware.com.br>
2021-09-09 11:43:26 -0300

    criado dbsets

--- LeakGas/src/LeakGas.Data/Context/LeakGasContext.cs
+++ LeakGas/src/LeakGas.Data/Context/LeakGasContext.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using LeakGas.Business.Models;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Text;
@@ -7,6 +8,22 @@ namespace LeakGas.Data.Context
 {
     public class LeakGasContext : DbContext
     {
+        #region DbSets
+        public DbSet<Acesso> Acesso { get; set; }
+        public DbSet<Apartamento> Apartamento { get; set; }
+        public DbSet<Cep> Cep { get; set; }
+        public DbSet<Cidade> Cidade { get; set; }
+        public DbSet<Condominio> Condominio { get; set; }
+        public DbSet<Estado> Estado { get; set; }
+        public DbSet<Login> Login { get; set; }
+        public DbSet<NivelAcesso> NivelAcesso { get; set; }
+        public DbSet<Notificacao> Notificacao { get; set; }
+        public DbSet<Ocorrencia> Ocorrencia { get; set; }
+        public DbSet<TipoOcorrencia> TipoOcorrencia { get; set; }
+        public DbSet<Usuario> Usuario { get; set; }
+        public DbSet<UsuarioCondominio> UsuarioCondominio { get; set; }
+
+        #endregion
         public LeakGasContext(DbContextOptions<LeakGasContext> options) : base(options)
         {
 
