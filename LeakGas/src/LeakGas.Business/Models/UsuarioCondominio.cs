691937d73db25e6932ccd0fefd1ed069dea6463d
Ruan Araújo <ruan.araujo@orsoftware.com.br>
2021-09-09 11:40:15 -0300

    finalizado criação de models

---
+++ LeakGas/src/LeakGas.Business/Models/UsuarioCondominio.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace LeakGas.Business.Models
+{
+    [Table("TB_USU_CON")]
+    public class UsuarioCondominio
+    {
+        [Column("ID_USU_CON")]
+        public int Id { get; set; }
+        [Column("FK_ID_USUARIO")]
+
+        public int IdUsuario { get; set; }
+        [Column("FK_ID_CONDOMINIO")]
+
+        public int IdCondominio { get; set; }
+    }
+}
