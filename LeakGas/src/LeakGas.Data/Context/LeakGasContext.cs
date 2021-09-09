1a4243009da38e1b0c27f9a742432b3c106b7322
Ruan Araújo <ruan.araujo@orsoftware.com.br>
2021-09-09 10:55:39 -0300

    criado contexto, injetando configs no contexto e instalção do pacote oracle ef core

---
+++ LeakGas/src/LeakGas.Data/Context/LeakGasContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeakGas.Data.Context
+{
+    public class LeakGasContext : DbContext
+    {
+        public LeakGasContext(DbContextOptions<LeakGasContext> options) : base(options)
+        {
+
+        }
+    }
+}
