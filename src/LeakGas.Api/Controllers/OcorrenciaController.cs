5e49eb1879c600761d61d3ffad877d13f2810fed
Ruan Araújo <ruanaraujo.c@gmail.com>
2021-09-10 19:51:57 -0300

    método de registrar vazamento na controller.

---
+++ src/LeakGas.Api/Controllers/OcorrenciaController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using LeakGas.Api.Wrapper;
+using LeakGas.Business.Interfaces;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Swashbuckle.AspNetCore.Annotations;
+
+namespace LeakGas.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class OcorrenciaController : MainController
+    {
+        public OcorrenciaController(INotificador notificador) : base(notificador)
+        {
+        }
+
+        [HttpPost("vazamento")]
+        [SwaggerResponse(200, type: typeof(Response))]
+        public async Task<ActionResult> RegistrarVazamento(int idApartamento)
+        {
+            if (!ModelState.IsValid) return CustomResponse(ModelState);
+
+            if (idApartamento == 0)
+            {
+                NotificarErro("Id incorreto.");
+                return CustomResponse();
+            }
+            return CustomResponse();
+        }
+    }
+}
