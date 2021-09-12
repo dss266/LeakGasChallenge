7402ffaf6ca1f40275baf145096ba87d00afe4b2
Ruan Araújo <ruanaraujo.c@gmail.com>
2021-09-12 00:35:32 -0300

    criado repositório de usuário

---
+++ src/LeakGas.Data/Repositories/UsuarioRepository.cs
@@ -0,0 +1,13 @@
+﻿using LeakGas.Business.Interfaces.Data;
+using LeakGas.Business.Models;
+using LeakGas.Data.Context;
+
+namespace LeakGas.Data.Repositories
+{
+    public class UsuarioRepository : Repository<Usuario>, IUsuarioRepository
+    {
+        public UsuarioRepository(LeakGasContext db) : base(db)
+        {
+        }
+    }
+}
